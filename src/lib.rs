9b13006a140b6ac13f1f1781c9cdd8fe549716e5
ananda kurnia <anandakurnia.dev@gmail.com>
2025-07-24 06:10:53 +0700

    feat: implement rough limit order matching process

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,23 @@ mod order_book {
     use ordered_float::OrderedFloat;
     use std::collections::BTreeMap;
 
+    #[allow(dead_code)]
     pub enum OrderType {
         LimitOrder,
     }
 
+    #[allow(dead_code)]
     pub enum OrderSide {
         Buy,
         Sell,
     }
 
+    #[allow(dead_code)]
     pub enum OrderLifeTime {
         GoodTilCancelOrder,
     }
 
+    #[allow(dead_code)]
     pub struct OrderRecord {
         pub order_side: OrderSide,
         pub order_id: String,
@@ -25,11 +29,13 @@ mod order_book {
         pub order_life_time: OrderLifeTime,
     }
 
+    #[allow(dead_code)]
     pub struct OrderEntry {
         pub queue: Vec<OrderRecord>,
         pub total_quantity: f64,
     }
 
+    #[allow(dead_code)]
     impl OrderEntry {
         fn new() -> Self {
             OrderEntry {
@@ -39,11 +45,13 @@ mod order_book {
         }
     }
 
+    #[allow(dead_code)]
     pub struct OrderBook {
         pub buy_orders: BTreeMap<OrderedFloat<f64>, OrderEntry>,
         pub sell_orders: BTreeMap<OrderedFloat<f64>, OrderEntry>,
     }
 
+    #[allow(dead_code)]
     impl OrderBook {
         pub fn new() -> Self {
             OrderBook {
@@ -52,38 +60,41 @@ mod order_book {
             }
         }
 
-        pub fn set_order(&mut self, order_record: OrderRecord) {
-            match order_record.order_side {
-                OrderSide::Buy => {
-                    let buy_orders = &mut self.buy_orders;
-                    if !buy_orders.contains_key(&OrderedFloat(order_record.price)) {
-                        buy_orders.insert(OrderedFloat(order_record.price), OrderEntry::new());
-                    }
+        pub fn set_order(&mut self, mut order_record: OrderRecord) {
+            let buy_orders = &mut self.buy_orders;
+            if !buy_orders.contains_key(&OrderedFloat(order_record.price)) {
+                buy_orders.insert(OrderedFloat(order_record.price), OrderEntry::new());
+            }
+            let buy_order_entry = buy_orders.get_mut(&OrderedFloat(order_record.price));
 
-                    let order_entry = buy_orders.get_mut(&OrderedFloat(order_record.price));
-                    match order_entry {
-                        Some(oe) => {
-                            let quantity = order_record.remaining_quantity;
-                            oe.queue.push(order_record);
-                            oe.total_quantity += quantity;
-                        }
-                        _ => {}
-                    }
-                }
-                OrderSide::Sell => {
-                    let sell_orders = &mut self.sell_orders;
-                    if !sell_orders.contains_key(&OrderedFloat(order_record.price)) {
-                        sell_orders.insert(OrderedFloat(order_record.price), OrderEntry::new());
-                    }
+            let sell_orders = &mut self.sell_orders;
+            if !sell_orders.contains_key(&OrderedFloat(order_record.price)) {
+                sell_orders.insert(OrderedFloat(order_record.price), OrderEntry::new());
+            }
+            let sell_order_entry = sell_orders.get_mut(&OrderedFloat(order_record.price));
 
-                    let order_entry = sell_orders.get_mut(&OrderedFloat(order_record.price));
-                    match order_entry {
-                        Some(oe) => {
+            if let (Some(buy), Some(sell)) = (buy_order_entry, sell_order_entry) {
+                match order_record.order_side {
+                    OrderSide::Buy => {
+                            if sell.total_quantity < order_record.remaining_quantity {
+                                order_record.remaining_quantity = order_record.remaining_quantity - sell.total_quantity;
+                                sell.total_quantity = 0.00;
+                            } 
+                            
+                            if sell.total_quantity > order_record.remaining_quantity {
+                                sell.total_quantity = sell.total_quantity - order_record.remaining_quantity;
+                                order_record.remaining_quantity = 0.0;
+                            }
+
+                            if order_record.remaining_quantity != 0.0 {
+                                buy.total_quantity = buy.total_quantity + order_record.remaining_quantity;
+                                buy.queue.push(order_record);                 
+                            }
+                    }
+                    OrderSide::Sell => {
                             let quantity = order_record.remaining_quantity;
-                            oe.queue.push(order_record);
-                            oe.total_quantity += quantity;
-                        }
-                        _ => {}
+                            sell.queue.push(order_record);
+                            sell.total_quantity += quantity;
                     }
                 }
             }
@@ -95,7 +106,7 @@ mod order_book {
         let mut orderbook = OrderBook::new();
 
         orderbook.set_order(OrderRecord {
-            order_side: OrderSide::Buy,
+            order_side: OrderSide::Sell,
             order_id: "1".to_string(),
             price: 100.00,
             initial_quantity: 100.00,
@@ -108,18 +119,8 @@ mod order_book {
             order_side: OrderSide::Buy,
             order_id: "2".to_string(),
             price: 100.00,
-            initial_quantity: 100.00,
-            remaining_quantity: 100.00,
-            order_type: OrderType::LimitOrder,
-            order_life_time: OrderLifeTime::GoodTilCancelOrder,
-        });
-
-        orderbook.set_order(OrderRecord {
-            order_side: OrderSide::Buy,
-            order_id: "3".to_string(),
-            price: 100.00,
-            initial_quantity: 100.00,
-            remaining_quantity: 100.00,
+            initial_quantity: 80.00,
+            remaining_quantity: 80.00,
             order_type: OrderType::LimitOrder,
             order_life_time: OrderLifeTime::GoodTilCancelOrder,
         });
@@ -127,9 +128,21 @@ mod order_book {
         let buy_order = orderbook.buy_orders.get(&OrderedFloat(100.0));
         match buy_order {
             Some(n) => {
-                assert_eq!(n.total_quantity, 300.0);
+                assert_eq!(n.total_quantity, 0.0);
+                assert_eq!(n.queue.len(), 0);
             }
             _ => {}
         }
+
+        let sell_order = orderbook.sell_orders.get(&OrderedFloat(100.0));
+        match sell_order {
+            Some(n) => {
+                assert_eq!(n.total_quantity, 20.0);
+                assert_eq!(n.queue.len(), 1);
+            }
+            _ => {}
+        }
+
+
     }
 }
